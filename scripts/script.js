d4d727d14c1f52671f120c5ec6ff098490d4cfd0
unifolia <james@electronicmail.ca>
2020-03-02 14:08:54 -0500

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,226 @@
+let three = {}
+
+let player = { 
+    height: 1.8, 
+    speed: 0.4, 
+    turnSpeed: 0.1, 
+}
+
+let scene = new THREE.Scene()
+let camera = new THREE.PerspectiveCamera(90, 1280 / 720, 0.1, 1000)
+let textureLoader = new THREE.TextureLoader()
+let isKeyDown = false
+
+three.createBackground = () => {
+    // Create floor
+    floor = new THREE.Mesh(
+        new THREE.PlaneGeometry(20, 20, 10, 10),
+        new THREE.MeshPhongMaterial({ 
+            color: 0xc4bcd0, 
+            wireframe: false})
+    )
+    floor.rotation.x -= Math.PI / 2
+
+    // Create ambient light
+    ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
+    
+    // Create direct light
+    light = new THREE.PointLight(0xffffff, 1, 25)
+    let { position, shadow } = light
+    position.set(-3, 6, -3)
+    shadow.camera.near = 0.1
+    shadow.camera.far = 25
+
+    // Set background
+    scene.background = new THREE.Color(0xebfaea)
+}
+
+three.createJames = () => {
+    let texture = textureLoader.load("./assets/jamesHeadshot.jpg")
+
+    james = new THREE.Mesh(
+        new THREE.BoxGeometry(5, 5, 5),
+        new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            map: texture,
+        })
+    )
+
+    james.position.set(-3, 5, 5)
+    james.receiveShadow = false
+    james.castShadow = true
+}
+
+three.createProjects = () => {
+    let pacTexture = textureLoader.load("./assets/pac-rem.png")
+    let cashTexture = textureLoader.load("./assets/cashComrade.png")
+    let marsTexture = textureLoader.load("./assets/marsometer.png")
+    let pathsTexture = textureLoader.load("./assets/pathsAndPodcasts.png")
+    let couchTexture = textureLoader.load("./assets/jamesCouch.png")
+
+    // 
+    pacRem = new THREE.Mesh(
+        new THREE.BoxGeometry(3, 3, 3),
+        new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            map: pacTexture,
+        })
+    )
+    pacRem.position.set(4, 5, 5)
+    pacRem.receiveShadow = false
+    pacRem.castShadow = false
+
+    // 
+    cashComrade = new THREE.Mesh(
+        new THREE.BoxGeometry(3, 3, 3),
+        new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            map: cashTexture,
+        })
+    )
+    cashComrade.position.set(7, 5, 0)
+    cashComrade.receiveShadow = false
+    cashComrade.castShadow = false
+
+    // 
+    marsometer = new THREE.Mesh(
+        new THREE.BoxGeometry(3, 3, 3),
+        new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            map: marsTexture,
+        })
+    )
+    marsometer.position.set(-8, 5, 0)
+    marsometer.receiveShadow = false
+    marsometer.castShadow = false
+
+    // 
+    paths = new THREE.Mesh(
+        new THREE.BoxGeometry(3, 3, 3),
+        new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            map: pathsTexture,
+        })
+    )
+    paths.position.set(10, 3, 5)
+    paths.receiveShadow = false
+    paths.castShadow = false
+
+    // 
+    jamesCouch = new THREE.Mesh(
+        new THREE.BoxGeometry(3, 3, 3),
+        new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            map: couchTexture,
+        })
+    )
+    jamesCouch.position.set(0, 2, -10)
+    jamesCouch.receiveShadow = false
+    jamesCouch.castShadow = false
+}
+
+three.createShapes = () => {
+    three.createJames()
+    three.createProjects()
+}
+
+three.createCanvas = () => {
+    camera.position.set(0, player.height, -5)
+    camera.lookAt(new THREE.Vector3(0, player.height, 0))
+
+    const canvas = document.querySelector('#canvas')
+    renderer = new THREE.WebGLRenderer({ canvas })
+
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.BasicShadowMap
+
+    document.body.appendChild(renderer.domElement)
+}
+
+three.resizeScreen = () => {
+    let resizeRendererToDisplaySize = renderer => {
+        let canvas = renderer.domElement
+        let width = canvas.clientWidth
+        let height = canvas.clientHeight
+        let needResize = canvas.width !== width || canvas.height !== height
+
+        if (needResize) {
+            renderer.setSize(width, height, false)
+        }
+        return needResize
+    }
+
+    if (resizeRendererToDisplaySize(renderer)) {
+        let canvas = renderer.domElement
+        camera.aspect = canvas.clientWidth / canvas.clientHeight
+        camera.updateProjectionMatrix()
+    }
+}
+
+three.animate = () => {
+    requestAnimationFrame(three.animate)
+
+    james.rotation.x += 0.001
+    james.rotation.y += 0.001
+    pacRem.rotation.x -= 0.001
+    pacRem.rotation.y -= 0.001
+    marsometer.rotation.y -= 0.001
+    cashComrade.rotation.z += 0.002
+    paths.rotation.z -= 0.002
+    paths.rotation.y += 0.002
+
+    three.resizeScreen()
+    renderer.render(scene, camera)
+}
+
+three.keyDown = () => {
+    document.addEventListener("keydown", event => {
+        isKeyDown = true
+
+        let { position, rotation } = camera
+        let { speed, turnSpeed } = player
+
+        if (isKeyDown) {
+            if (event.keyCode == 87) {
+                // "W" - move forward
+                position.x -= Math.sin(rotation.y) * speed
+                position.z -= -Math.cos(rotation.y) * speed
+            } else if (event.keyCode == 65) {
+                // "A" - move left
+                position.x += Math.sin(rotation.y + Math.PI / 2) * speed
+                position.z += -Math.cos(rotation.y + Math.PI / 2) * speed
+            } else if (event.keyCode == 83) {
+                // "S" - move backward
+                position.x += Math.sin(rotation.y) * speed;
+                position.z += -Math.cos(rotation.y) * speed;
+            } else if (event.keyCode == 68) {
+                // "D" - move right
+                position.x += Math.sin(rotation.y - Math.PI / 2) * speed
+                position.z += -Math.cos(rotation.y - Math.PI / 2) * speed
+            } else if (event.keyCode == 37) {
+                // "<" - turn left
+                rotation.y -= turnSpeed
+            } else if (event.keyCode == 39) {
+                // ">" - turn right
+                rotation.y += turnSpeed
+            }
+        }
+    })
+}
+
+three.keyUp = () => {
+    isKeyDown = false
+}
+
+let init = () => {
+    three.createCanvas()
+    three.createBackground()
+    three.createShapes()
+
+    scene.add(james, pacRem, marsometer, cashComrade, paths, jamesCouch, floor, ambientLight, light)
+
+    three.animate()
+    three.keyDown()
+}
+
+window.onload = init
